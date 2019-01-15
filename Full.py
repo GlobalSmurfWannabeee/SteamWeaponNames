d12d5c9d8b6fcb98178cf5ce45a899c4ab77ce55
GlobalSmurfWannabeee <46648384+GlobalSmurfWannabeee@users.noreply.github.com>
2019-01-15 18:24:18 +0530

    Add files via upload

--- Full.py
+++ Full.py
@@ -20,11 +20,41 @@ def getResult(link):
 				z=q.h3.text
 				p=q.find("a",{"class":"nounderline"})
 				print(z+" Type: "+p['title'].replace("All","").replace("Skins",""))
+
+				##
+				priceLink1=q.find("div",{"class":"details-link"})
+				ll=priceLink1.p.a['href']
+				getPrice(ll)
+				##
+
 			except(TypeError, KeyError) as e:
 				pass
 
 ##########################################################
 
+
+##########################################################
+
+
+def getPrice(link):
+	source=requests.get(link).text
+	soup=BeautifulSoup(source,'html.parser')
+
+	x=soup.find("tbody")
+	z=x.findAll("tr")
+	print("")
+	for i in z:
+		try:
+			q=i.find('td',{"class":"text-left"})
+			o=i.find('td',{"class":"text-right"})
+			print(q.text.strip()+" "+o.text.strip())
+		except(NameError,TypeError) as e:
+			pass
+	print("")
+
+
+
+##############################################################
 
 
 source=requests.get("https://csgostash.com/").text
@@ -39,7 +69,9 @@ for x in AllWeapon:
 	y=x.find("ul",{"class":"dropdown-menu navbar-dropdown-large"})
 	for li in y.findAll("li"):
 		try:
+			print("")
 			print(li.a['href'].replace("https://csgostash.com/weapon/","Weapon : "))
+			print("")
 			getResult(li.a['href'])
 		except(TypeError,NameError) as e:
 			pass
